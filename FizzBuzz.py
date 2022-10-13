fc767c86a10a8ab6af841a91cecb25e6c86ec697
LukeOlson01 <89668519+LukeOlson01@users.noreply.github.com>
2022-10-13 11:57:59 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,9 @@
-
+for x in range(1, 101):
+  if x % 3 == 0:
+    print('Fizz')
+for x in range(1, 101):
+  if x % 5 == 0:
+    print('Buzz')
+for x in range(1, 101):
+  if x % 15 == 0:
+    print('FizzBuzz')
